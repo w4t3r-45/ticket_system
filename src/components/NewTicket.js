dfb6533202911428f07060eec4acc1559cd80ce3
said-pc <webhost48@gmail.com>
2021-10-01 14:55:20 +0100

    fixed add ticket modal UI

--- src/components/NewTicket.js
+++ src/components/NewTicket.js
@@ -1,18 +1,160 @@
-import { Modal } from "@mui/material";
+import { useState } from "react";
+import {
+  Modal,
+  Box,
+  Paper,
+  Typography,
+  styled,
+  TextField,
+  Select,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Button,
+  Divider
+} from "@mui/material";
+import { Close, ConfirmationNumber } from "@mui/icons-material";
+import { useTheme } from "@mui/material/styles";
 import { useSelector, useDispatch } from "react-redux";
 import { closeNewTicket } from "../redux/actions/actions";
+import HeadShake from "react-reveal/HeadShake";
+import { useDropzone } from "react-dropzone";
 
 export default function NewTicket(props) {
+  const theme = useTheme();
   const dispatch = useDispatch();
   const open = useSelector((state) => state.ui.openTkt);
   const handleClose = (event) => {
     dispatch(closeNewTicket());
   };
+
+  // const Form = styled("form")(({ theme }) => ({
+  //   padding: 2
+  // }));
+
+  const [field, setField] = useState("");
+  const [importance, setImportance] = useState("");
+
+  const handleChange = (event) => {
+    switch (event.target.name) {
+      case "importance":
+        setImportance(event.target.value);
+        break;
+      case "field":
+        setField(event.target.value);
+        break;
+      default:
+        console.log("no one reached");
+        return;
+    }
+  };
+
   return (
     <>
       {console.log(props)}
-      <Modal open={open} onClose={(event) => handleClose(event)}>
-        <h1>hello from modal</h1>
+      <Modal
+        component={Box}
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        open={open}
+        onClose={(event) => handleClose(event)}
+      >
+        <HeadShake>
+          <Box component={Paper} elevation={1} sx={{ width: 450 }}>
+            <form>
+              <Box
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+                sx={{
+                  height: 50,
+                  bgcolor: theme.palette.primary.main,
+                  color: "#fff",
+                  pl: 2,
+                  pr: 2,
+                  mb: 3
+                }}
+              >
+                <Box display="flex" alignItems="center">
+                  <Typography variant="h4" fontSize="1.4rem" mr={1}>
+                    New Ticket
+                  </Typography>
+                  <ConfirmationNumber />
+                </Box>
+                <Close
+                  cursor="pointer"
+                  onClick={(event) => handleClose(event)}
+                />
+              </Box>
+              <Box sx={{ pl: 2, pt: 1, pb: 1, pr: 2 }}>
+                <TextField
+                  size="small"
+                  sx={{ mb: 2 }}
+                  fullWidth
+                  label="Ticket Title"
+                  key="x1s3"
+                />
+
+                <FormControl size="small" sx={{ mb: 2 }} fullWidth>
+                  <InputLabel id="field-select-label">Ticket Field</InputLabel>
+                  <Select
+                    labelId="field-select-label"
+                    id="field-select"
+                    name="field"
+                    value={field}
+                    label="Ticket Field"
+                    onChange={(event) => handleChange(event)}
+                  >
+                    <MenuItem value={"IT"}>IT </MenuItem>
+                    <MenuItem value={"SYS"}>System</MenuItem>
+                    <MenuItem value={"HR"}>Human Resources</MenuItem>
+                    <MenuItem value={"TS"}>Technical Support</MenuItem>
+                    <MenuItem value={"TLNT"}>Talents</MenuItem>
+                  </Select>
+                </FormControl>
+
+                <FormControl size="small" sx={{ mb: 2 }} fullWidth>
+                  <InputLabel id="importance-select-label">
+                    Level of importance
+                  </InputLabel>
+                  <Select
+                    labelId="importance-select-label"
+                    id="importance-select"
+                    value={importance}
+                    name="importance"
+                    label="Level of importance"
+                    onChange={(event) => handleChange(event)}
+                  >
+                    <MenuItem value={"SP"}>Super Urgent </MenuItem>
+                    <MenuItem value={"HG"}>High </MenuItem>
+                    <MenuItem value={"MD"}>Medium</MenuItem>
+                    <MenuItem value={"LW"}>Low</MenuItem>
+                  </Select>
+                </FormControl>
+
+                <TextField
+                  size="small"
+                  sx={{ mb: 2 }}
+                  fullWidth
+                  multiline
+                  maxRows={5}
+                  label="Description"
+                  key="x12asasaf3"
+                />
+                <Divider>Please Be Sure Of Your Entered Informations</Divider>
+                <Button
+                  variant="outlined"
+                  type="submit"
+                  sx={{ mt: 2 }}
+                  fullWidth
+                >
+                  Validate Ticket
+                </Button>
+              </Box>
+            </form>
+          </Box>
+        </HeadShake>
       </Modal>
     </>
   );
--- src/themes/rallyTheme.js
+++ src/themes/rallyTheme.js
@@ -37,11 +37,6 @@ export const rallyLight = createTheme({
       fontWeight: "300",
       fontSize: 60
     },
-    h3: {
-      fontFamily: "'Eczar', serif",
-      fontWeight: "400",
-      fontSize: 48
-    }, //ECZAR
     h4: {
       fontFamily: " 'Roboto Condensed', sans-serif",
       fontWeight: "300",
@@ -67,11 +62,6 @@ export const rallyLight = createTheme({
       fontWeight: "400",
       fontSize: 14
     },
-    body1: {
-      fontFamily: " 'Eczar', serif",
-      fontWeight: "400",
-      fontSize: 16
-    }, //ECZAR
     body2: {
       fontFamily: " 'Roboto Condensed', sans-serif",
       fontWeight: "300",
@@ -100,6 +90,11 @@ export const rallyLight = createTheme({
     "none", //elevation(0) represented by shadows[0]
     "0px 2px 1px -10px rgba(0,0,0,0.1),0px 1px 2px 0px rgba(0,0,0,0.1),0px 1px 2px 0px rgba(0,0,0,0.1)", //elevation(1)
     "0px 3px 1px -2px rgba(0,0,0,0.2),0px 2px 2px 0px rgba(0,0,0,0.14),0px 1px 5px 0px rgba(0,0,0,0.12)", //elevation(2)
-    "0px 3px 10px -20px rgba(0,0,0,0.1),0px 3px 20px 0px rgba(0,0,0,0.1),0px 1px 10px 0px rgba(0,0,0,0.1)" //elevation(3)
+    "0px 3px 10px -20px rgba(0,0,0,0.1),0px 3px 20px 0px rgba(0,0,0,0.1),0px 1px 10px 0px rgba(0,0,0,0.1)", //elevation(3)
+    "0px 2px 4px -1px rgba(0, 0, 0, 0.2),0px 4px 5px 0px rgba(0, 0, 0, 0.14),0px 1px 10px 0px rgba(0, 0, 0, 0.12)",
+    "0px 3px 5px -1px rgba(0, 0, 0, 0.2),0px 5px 8px 0px rgba(0, 0, 0, 0.14),0px 1px 14px 0px rgba(0, 0, 0, 0.12)",
+    "0px 3px 5px -1px rgba(0, 0, 0, 0.2),0px 6px 10px 0px rgba(0, 0, 0, 0.14),0px 1px 18px 0px rgba(0, 0, 0, 0.12)",
+    "0px 4px 5px -2px rgba(0, 0, 0, 0.2),0px 7px 10px 1px rgba(0, 0, 0, 0.14),0px 2px 16px 1px rgba(0, 0, 0, 0.12)",
+    "0px 5px 5px -3px rgba(0, 0, 0, 0.2),0px 8px 10px 1px rgba(0, 0, 0, 0.14),0px 3px 14px 2px rgba(0, 0, 0, 0.12)"
   ]
 });
